9dbcaeb95f0c7508d688e254dbadea78d5ac12c5
Felipe Cook <felipepcook@gmail.com>
2019-03-10 17:30:15 -0600

    Added for loops

--- src/edu/cnm/deepdive/Matrices.java
+++ src/edu/cnm/deepdive/Matrices.java
@@ -2,9 +2,26 @@ package edu.cnm.deepdive;
 
 public class Matrices {
 
+  public static void main(String[] args) {
+    Matrices matrices = new Matrices();
+    System.out.println(matrices.rotate());
+  }
+
   public static int[][] rotate(int[][] data) {
 
     int[][] returnData = new int[data.length][data.length];
+
+    for (int i = 0; i < data.length; i++) {
+      for (int j = 0; j < data.length; j++) {
+        int[][] tempData = new int[i][j];
+        returnData[1][0] = data[0][0];
+        returnData[0][0] = data[0][1];
+        returnData[1][1] = data[1][0];
+        returnData[0][1] = data[1][1];
+
+
+      }
+    }
 
   }
 
